81e523b2529c67fdb3ba973d68ec0d5f50e378f5
bouzidnm <simab0418@gmail.com>
2015-04-15 19:31:50 -0700

    A more generalized pipeline (for institutions) to extract specimens to be measured

--- S_occidentalis_data.R
+++ S_occidentalis_data.R
@@ -11,41 +11,41 @@ specimens <- table(museums)
 specimen_hist <- barplot(specimens, axes=T, axisnames=T, ylab="# specimens", main=expression(paste(italic("Sceloporus occidentalis "), "by Institution")), las=2)
 mtext("Institution", side=1, line=5)
 
-### Subdivide by Institution & export as .csv
-## occ_data$occurrenceRemarks!=""
-occ_CAS <- occ_data[occ_data$institutionCode=="CAS",]
+## Specify institution from which you want records, targeted prep type
+iCode <- "CAS"
+prep <- "Alcohol" # Alcohol, Dry, Tissue; only works for CAS specimens
+
+### Subdivide by Institution
+#occ_CAS <- occ_data[occ_data$institutionCode=="CAS",]
+occ_rawData <- occ_data[occ_data$institutionCode==iCode,]
 
 ## Things with SVL already measured; output .csv file
-occ_CAS_SVL <- grep("\\<SVL\\>", occ_CAS$occurrenceRemarks)
-occ_CAS_SVL_measured <- occ_CAS[occ_CAS_SVL,]
-write.csv(occ_CAS_SVL_measured, file="CAS_SVL_measured.csv")
-occ_CAS <- occ_CAS[-occ_CAS_SVL,]
+occ_SVL <- grep("\\<SVL\\>", occ_rawData$occurrenceRemarks)
+occ_SVL_measured <- occ_rawData[occ_SVL,]
+write.csv(occ_SVL_measured, file=paste(iCode,"_SVL_measured.csv", sep=""))
+occ_unmeasured <- occ_rawData[-occ_SVL,]
 
 ## Alcohol specimens only
-occ_CAS_fluid <-  grep("\\<Alcohol\\>", occ_CAS$preparations)
-occ_CAS <- occ_CAS[occ_CAS_fluid,]
+occ_prep <-  grep(prep, occ_unmeasured$preparations)
+#occ_CAS_fluid <-  grep("\\<Alcohol\\>", occ_unmeasured$preparations) # grep a single word in a string
+occ_unmeasured <- occ_unmeasured[occ_prep,]
+#write.csv(occ_unmeasured, file="test.csv") # just a test
 
 # Not lost, missing, or destroyed
-occ_CAS_lost <- grep("\\<Lost\\>", occ_CAS$occurrenceRemarks)
-occ_CAS <- occ_CAS[-occ_CAS_lost,]
-occ_CAS_missing <- grep("\\<Missing\\>", occ_CAS$occurrenceRemarks)
-occ_CAS <- occ_CAS[-occ_CAS_missing,]
-occ_CAS_destroyed <- grep("\\<Destroyed\\>", occ_CAS$occurrenceRemarks)
-occ_CAS_toMeasure <- occ_CAS[-occ_CAS_destroyed,]
+occ_lost <- grep("\\<Lost\\>", occ_unmeasured$occurrenceRemarks)
+occ_missing <- grep("\\<Missing\\>", occ_unmeasured$occurrenceRemarks)
+occ_destroyed <- grep("\\<Destroyed\\>", occ_unmeasured$occurrenceRemarks)
+occ_gone <- c(occ_lost, occ_missing, occ_destroyed)
+occ_toMeasure <- occ_unmeasured[-occ_gone,]
 
-# Remove columns with all NAs
-occ_CAS_toMeasure <- occ_CAS_toMeasure[, unlist(lapply(occ_CAS_toMeasure, function(x) !all(is.na(x))))]
+# Remove columns with all NAs; optional, just to clean data frame
+occ_toMeasure <- occ_toMeasure[, unlist(lapply(occ_toMeasure, function(x) !all(is.na(x))))]
 
-## Write .csv of specimens to measure
-write.csv(occ_CAS_toMeasure, file="CAS_SVL_toMeasure.csv")
+## Remove all records without lat/long; optional
+occ_toMeasure <- subset(occ_toMeasure, decimalLatitude!="NA" & decimalLongitude!="NA")
 
-## Remove all records without lat/long
-CAS_toMeasure <- read.csv("CAS_SVL_toMeasure.csv")
-CAS <- subset(CAS_toMeasure, decimalLatitude!="NA" & decimalLongitude!="NA")
-write.csv(CAS, file="CAS_toMeasure.csv")
-write.csv(scelop_subsets, file="scelop_subsets.csv")
-
-
+## Write .csv of specimens to measure for institution specified in iCode
+write.csv(occ_toMeasure, file=paste(iCode,"_SVL_toMeasure.csv",sep=""))
 
 
 
